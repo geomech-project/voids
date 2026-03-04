64e3aba1a0280971467f5df0bc460f6f9a93ed4c
Diego Volpatto <volpatto@lncc.br>
2026-03-04 14:23:36 -0300

    Add characteristic size functions and update __all__ exports

--- src/voids/geom/__init__.py
+++ src/voids/geom/__init__.py
@@ -1,3 +1,9 @@
+from voids.geom.characteristic import area_equivalent_diameter, characteristic_size
 from voids.geom.hydraulic import available_conductance_models, throat_conductance
 
-__all__ = ["available_conductance_models", "throat_conductance"]
+__all__ = [
+    "area_equivalent_diameter",
+    "characteristic_size",
+    "available_conductance_models",
+    "throat_conductance",
+]
