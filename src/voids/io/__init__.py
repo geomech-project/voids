d39dbbb7df1a8bb92a1613e19b4b70922f35934a
Diego Volpatto <volpatto@lncc.br>
2026-05-09 02:50:56 -0300

    Add volume data handling and surface mesh functionality

--- src/voids/io/__init__.py
+++ src/voids/io/__init__.py
@@ -2,6 +2,17 @@ from voids.io.porespy import ensure_cartesian_boundary_labels, from_porespy, sca
 from voids.io.hdf5 import save_hdf5, load_hdf5
 from voids.io.openpnm import to_openpnm_dict, to_openpnm_network
 from voids.io.pnflow_cnm import PnflowCNMImportResult, load_pnflow_cnm
+from voids.io.volume import (
+    SurfaceMesh,
+    VolumeData,
+    load_surface_mesh,
+    load_volume,
+    load_volume_data,
+    save_surface_mesh,
+    save_volume,
+    save_volume_bundle,
+    surface_mesh_from_binary_volume,
+)
 
 __all__ = [
     "ensure_cartesian_boundary_labels",
@@ -13,4 +24,13 @@ __all__ = [
     "to_openpnm_network",
     "PnflowCNMImportResult",
     "load_pnflow_cnm",
+    "SurfaceMesh",
+    "VolumeData",
+    "load_surface_mesh",
+    "load_volume",
+    "load_volume_data",
+    "save_surface_mesh",
+    "save_volume",
+    "save_volume_bundle",
+    "surface_mesh_from_binary_volume",
 ]
